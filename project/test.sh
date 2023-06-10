82c9eefe1e1fcf5bf635686b3f38fe360fb6a7cf
Felix Büppelmann <fbueppelmann@gmail.com>
2023-06-10 21:53:25 +0200

    Instead of conda use pip in github actions

--- project/test.sh
+++ project/test.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Create a Conda environment from conda.yaml
-conda env create --name testing_SAMSE_env --file project/conda.yaml
-
-# Activate the Conda environment
-conda activate testing_SAMSE_env
-
-# Install pytest (if not already installed)
-pip install pytest
+pip install -r project/requirements.txt
 
 # Run the pytest script
 python -m pytest project/test/test_ETLPipeline.py -v
 
-# Deactivate the Conda environment
-conda deactivate
-
-# Delete the Conda environment
-conda env remove --name testing_SAMSE_env
