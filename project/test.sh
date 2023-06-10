ff9328add7cde3885fdedc85a0357a80c370cc77
Felix Büppelmann <fbueppelmann@gmail.com>
2023-06-10 21:55:19 +0200

    Changed location of requirements

--- project/test.sh
+++ project/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -r project/requirements.txt
+pip install -r project/test/test_requirements.txt
 
 # Run the pytest script
 python -m pytest project/test/test_ETLPipeline.py -v
