c98a203a485032c430b703b76c5e3a9eeac0eb0b
Felix Büppelmann <fbueppelmann@gmail.com>
2023-06-14 17:26:45 +0200

    Update exercise

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -38,13 +38,16 @@ df["others"] = pd.to_numeric(df["others"], errors="coerce")
 # Validate and clean the data
 df = df[pd.to_numeric(df["CIN"], errors="coerce").notnull()]  # Filter valid CINs
 df = df.astype({"CIN": str})  # Convert CIN column to string
-df = df[df.select_dtypes(include="number").ge(0).all(1)]  # Filter rows with positive integer values
+
+# Drop rows with negative values and replace null values with 0
+df = df[df.select_dtypes(include=["float64", "int64"]).ge(0).all(axis=1)]
+df.fillna(0, inplace=True)
 
 # Create a connection to the SQLite database using SQLAlchemy
 engine = create_engine(f"sqlite:///{database_path}")
 
 # Write the DataFrame into a SQLite table called "cars"
-df.to_sql("cars1", engine, if_exists="replace", index=False)
+df.to_sql("cars", engine, if_exists="replace", index=False)
 
 # Close the database connection
 engine.dispose()
