3b0fd2820adfe9e2a05c0ddb7e0788323e6eba1e
Felix Büppelmann <fbueppelmann@gmail.com>
2023-07-05 19:28:32 +0200

    update ex5

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -1,7 +1,9 @@
 import urllib.request
 import zipfile
 import csv
-import sqlite3
+from sqlalchemy import create_engine, Column, String, Float, BigInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # Download the GTFS data
 url = 'https://gtfs.rhoenenergie-bus.de/GTFS.zip'
@@ -13,34 +15,53 @@ with zipfile.ZipFile('GTFS.zip', 'r') as zip_ref:
 
 # Define the desired columns and their data types
 desired_columns = ['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'zone_id']
-desired_data_types = ['TEXT', 'TEXT', 'FLOAT', 'FLOAT', 'BIGINT']
+desired_data_types = [String, String, Float, Float, BigInteger]
 
 # Filter and validate the data
 filtered_data = []
 with open('GTFS/stops.txt', 'r', encoding='utf-8-sig') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        if row['zone_id'] == '2001' and row['stop_name'].isascii() and \
-           -90 <= float(row['stop_lat']) <= 90 and -90 <= float(row['stop_lon']) <= 90:
+        if (
+            row['zone_id'] == '2001'
+            and row['stop_name'].isascii()
+            and -90 <= float(row['stop_lat']) <= 90
+            and -90 <= float(row['stop_lon']) <= 90
+        ):
             filtered_data.append(row)
 
-# Create the SQLite database and insert the data
-connection = sqlite3.connect('gtfs.sqlite')
-cursor = connection.cursor()
+# Create the SQLite database and table using SQLAlchemy
+Base = declarative_base()
 
-# Create the stops table with desired column names and types
-columns_str = ', '.join(f'{col} {data_type}' for col, data_type in zip(desired_columns, desired_data_types))
-create_table_query = f'CREATE TABLE stops ({columns_str})'
-cursor.execute(create_table_query)
+
+class Stop(Base):
+    __tablename__ = 'stops'
+    stop_id = Column(String, primary_key=True)
+    stop_name = Column(String)
+    stop_lat = Column(Float)
+    stop_lon = Column(Float)
+    zone_id = Column(BigInteger)
+
+
+# Create the database engine and session
+engine = create_engine('sqlite:///gtfs.sqlite')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # Insert the filtered data into the stops table
 for row in filtered_data:
-    values_str = ', '.join('?' * len(desired_columns))
-    insert_query = f'INSERT INTO stops ({", ".join(desired_columns)}) VALUES ({values_str})'
-    cursor.execute(insert_query, tuple(row[col] for col in desired_columns))
+    stop = Stop(
+        stop_id=row['stop_id'],
+        stop_name=row['stop_name'],
+        stop_lat=float(row['stop_lat']),
+        stop_lon=float(row['stop_lon']),
+        zone_id=int(row['zone_id'])
+    )
+    session.add(stop)
 
-# Commit the changes and close the connection
-connection.commit()
-connection.close()
+# Commit the changes and close the session
+session.commit()
+session.close()
 
 print("Data pipeline completed successfully!")
