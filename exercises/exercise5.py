c694033f3cd7615785debf5df110d9ed9963cc3f
Felix Büppelmann <fbueppelmann@gmail.com>
2023-07-09 14:15:37 +0200

    check wether exercise feedback was updated

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -20,6 +20,7 @@ with open('GTFS/stops.txt', 'r', encoding='utf-8-sig') as csv_file:
     for row in csv_reader:
         if (
             row['zone_id'] == '2001'
+            and row['stop_name'].isascii()
             and -90 <= float(row['stop_lat']) <= 90
             and -90 <= float(row['stop_lon']) <= 90
         ):
